af98386d45d6cd2b445ea55262158d7c90222a0a
cwnuiot <127716457+cwnuiot@users.noreply.github.com>
2023-05-13 15:29:57 +0800

    Add files via upload

---
+++ SCUT-TRAIN.py
@@ -0,0 +1,124 @@
+import torch
+import time
+import os
+from torch.autograd import Variable
+import numpy as np
+from model import TwoStreamTransformer
+import torch.nn as nn
+import time
+from torch.utils.data import Dataset,DataLoader
+data_fixed_length=141
+def savedata(root):
+    image, image1,lebal = [], [],[]
+    vecs_pt = torch.load(root)
+    image,image1,lebal=vecs_pt[0],vecs_pt[1],vecs_pt[2]
+    c=[image, image1, lebal]
+    return c
+class Rumor_Data(Dataset):
+    def __init__(self, dataset):
+        self.text = dataset[0]
+        self.image = dataset[1]
+        #self.social_context = torch.from_numpy(np.array(dataset['social_feature']))
+        self.label = dataset[2]
+        print('数据长',len(self.label))
+
+    def __len__(self):
+        return len(self.label)
+    def __getitem__(self, idx):
+        return (self.text[idx], self.image[idx]), self.label[idx]
+traindir = "Couch_GB2/trnCouch_GB2.pth"
+testdir = "Couch_GB2/tstCouch_GB2.pth"
+time0=time.time()
+train=savedata(traindir)
+test=savedata(testdir)
+train_dataset = Rumor_Data(train)
+test_dataset = Rumor_Data(test)
+time1 = time.time()
+print('加载时间：',time1 - time0)
+lr = 0.002
+seq_length = int(1120)
+certion = nn.CrossEntropyLoss()
+batch_size = 256
+train_loader = DataLoader(dataset=train_dataset,
+                              batch_size=batch_size,
+                              shuffle=True)
+test_loader = DataLoader(dataset=test_dataset,
+                             batch_size=batch_size,
+                             shuffle=False)
+if torch.cuda.is_available():
+    print('use gpu')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device('cuda')
+permute = torch.Tensor(np.random.permutation(1040).astype(np.float64)).long()
+model = TwoStreamTransformer(3008)
+model = model.to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+from torch.optim.lr_scheduler import MultiStepLR
+lr_scheduler = MultiStepLR(optimizer,
+                               milestones=[20,35],
+                               gamma=0.1)
+certion = nn.CrossEntropyLoss()
+lrlist=[]
+def train(ep):
+    global steps
+    model.train()
+    train_loss = 0
+    now = time.time()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        train_txt1,train_txt2,target=Variable(data[0].to(device)),Variable(data[1].to(device)),Variable(target.to(device))
+        optimizer.zero_grad()
+        #print('1111',train_txt1.size())
+        train_txt1 = train_txt1.view(-1, 1, seq_length)
+        train_txt2 = torch.unsqueeze(train_txt2, dim=1)
+        #print('aaa',train_txt2.size())
+        output = model(train_txt1,train_txt2)
+        output = torch.squeeze(output, dim=0)
+        loss = certion(output, target)
+        loss.backward()
+        optimizer.step()
+        train_loss += loss
+        #test(ep)
+    traintime = time.time()
+    print(traintime - now)
+    lr_scheduler.step(ep)
+    #print(lr)
+    lrlist.append(lr)
+    print('学习率',lr)
+acclist=[]
+def test(ep):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    if ep ==1:
+        torch.save(model.state_dict(),'papernet5.mdl')
+    with torch.no_grad():
+        for batch_idx, (data, target) in enumerate(test_loader):
+            starttime = time.time()
+            test_txt1,test_txt2,target=Variable(data[0].to(device)),Variable(data[1].to(device)),Variable(target.to(device))
+            test_txt1 = test_txt1.view(-1, 1, seq_length)
+            test_txt2=torch.unsqueeze(test_txt2,dim=1)
+            output = model(test_txt1,test_txt2).to(device)
+            endtime = time.time()
+#跑完程序后的时间-跑程序前的时间，结果保留两位浮数点
+            #print(f"It took {endtime-starttime:.3f} seconds to compute")
+            test_loss += certion(output, target).item()
+            pred = output.data.max(1, keepdim=True)[1]
+            correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+        test_loss /= len(test_loader.dataset)
+        print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.4f}%)\n'.format(
+            test_loss, correct, len(test_loader.dataset),
+            100. * correct / len(test_loader.dataset)))
+        acc=float(correct / len(test_loader.dataset))
+        acclist.append(acc)
+        print(acclist)
+        print(max(acclist))
+        return test_loss
+epochs =45
+if __name__ == '__main__':
+    for epoch in range(1, epochs + 1):
+        print(epoch)
+        time3=time.time()
+        train(epoch)
+        test(epoch)
+        time4=time.time()
+        print('训练时间',time4-time3)
---
+++ model.py
@@ -0,0 +1,193 @@
+import torch
+import torch.nn.functional as F
+import numpy as np
+import math
+import torch.nn as nn
+from Visiontrans import ViT
+class GELU(nn.Module):
+    def __init__(self):
+        super(GELU, self).__init__()
+
+    def forward(self, x):
+        return 0.5 * x * (1 + F.tanh(np.sqrt(2 / np.pi) * (x + 0.044715 * torch.pow(x, 3))))
+def drop_path(x, drop_prob: float = 0., training: bool = False):
+    if drop_prob == 0. or not training:
+        return x
+    keep_prob = 1 - drop_prob
+    shape = (x.shape[0],) + (1,) * (x.ndim - 1)
+    random_tensor = keep_prob + torch.rand(shape, dtype=x.dtype, device=x.device)
+    random_tensor.floor_()
+    output = x.div(keep_prob) * random_tensor
+    return output
+class DropPath(nn.Module):
+    def __init__(self, drop_prob=None):
+        super(DropPath, self).__init__()
+        self.drop_prob = drop_prob
+    def forward(self, x):
+        return drop_path(x, self.drop_prob, self.training)
+class PatchEmbed(nn.Module):
+    def __init__(self, input_shape=[1, 1040], patch_size=16, in_chans=1, num_features=65, norm_layer=None,
+                 flatten=True):
+        super().__init__()
+        self.num_patches = 8
+        self.flatten = flatten
+        self.proj = nn.Conv1d(in_chans, num_features, kernel_size=patch_size, stride=patch_size)
+        self.norm = norm_layer(num_features) if norm_layer else nn.Identity()
+    def forward(self, x):
+        x = self.proj(x)
+        if self.flatten:
+            x = x.flatten(2).transpose(1, 2)
+        x = self.norm(x)
+        return x
+class Attention(nn.Module):
+    def __init__(self, dim, num_heads=8, qkv_bias=False, attn_drop=0., proj_drop=0.):
+        super().__init__()
+        self.num_heads = num_heads
+        self.scale = (dim // num_heads) ** -0.5
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+    def forward(self, x):
+        B, N, C = x.shape
+        # print(B, N, C)
+        # print('9898989')
+        # print(x.size())
+        # print(self.qkv(x).size())
+        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv[0], qkv[1], qkv[2]
+        # print(q, k, v)
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
+class Mlp(nn.Module):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=GELU, drop=0.):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        drop_probs = (drop, drop)
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.drop1 = nn.Dropout(drop_probs[0])
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop2 = nn.Dropout(drop_probs[1])
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop1(x)
+        x = self.fc2(x)
+        x = self.drop2(x)
+        return x
+class Block(nn.Module):
+    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, drop=0., attn_drop=0.,
+                 drop_path=0., act_layer=GELU, norm_layer=nn.LayerNorm):
+        super().__init__()
+        self.norm1 = norm_layer(dim)
+        self.attn = Attention(dim, num_heads=num_heads, qkv_bias=qkv_bias, attn_drop=attn_drop, proj_drop=drop)
+        self.norm2 = norm_layer(dim)
+        self.mlp = Mlp(in_features=dim, hidden_features=int(dim * mlp_ratio), act_layer=act_layer, drop=drop)
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+    def forward(self, x):
+        x = x + self.drop_path(self.attn(self.norm1(x)))
+        x = x + self.drop_path(self.mlp(self.norm2(x)))
+        return x
+from functools import partial
+class VisionTransformer(nn.Module):
+    def __init__(
+            self, input_shape=[1, 1120], patch_size=16, in_chans=1, num_classes=256, num_features=108,
+            depth=12, num_heads=12, mlp_ratio=4., qkv_bias=True, drop_rate=0.1, attn_drop_rate=0.1, drop_path_rate=0.1,
+            norm_layer=partial(nn.LayerNorm, eps=1e-6), act_layer=GELU
+    ):
+        super().__init__()
+        self.patch_embed = PatchEmbed(input_shape=input_shape, patch_size=patch_size, in_chans=in_chans,
+                                      num_features=num_features)
+        num_patches = 70
+        self.num_features = num_features
+        self.new_feature_shape = [1, int(input_shape[1] // patch_size)]
+        self.old_feature_shape = [1, int(input_shape[1] // patch_size)]
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, num_features))
+        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches + 1, num_features))
+        self.pos_drop = nn.Dropout(p=drop_rate)
+        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, depth)]
+        self.blocks = nn.Sequential(
+            *[
+                Block(
+                    dim=num_features,
+                    num_heads=num_heads,
+                    mlp_ratio=mlp_ratio,
+                    qkv_bias=qkv_bias,
+                    drop=drop_rate,
+                    attn_drop=attn_drop_rate,
+                    drop_path=dpr[i],
+                    norm_layer=norm_layer,
+                    act_layer=act_layer
+                ) for i in range(depth)
+            ]
+        )
+        self.norm = norm_layer(num_features)
+        self.head = nn.Linear(num_features, num_classes)  # if num_classes > 0 else nn.Identity()
+    def forward_features(self, x):
+        x = self.patch_embed(x)
+        cls_token = self.cls_token.expand(x.shape[0], -1, -1)
+        x = torch.cat((cls_token, x), dim=1)
+        cls_token_pe = self.pos_embed[:, 0:1, :]
+        img_token_pe = self.pos_embed[:, 1:, :]
+        img_token_pe = img_token_pe.view(1, *self.old_feature_shape, -1).permute(0, 3, 1, 2)
+        img_token_pe = F.interpolate(img_token_pe, size=self.new_feature_shape, mode='bicubic', align_corners=False)
+        img_token_pe = img_token_pe.permute(0, 2, 3, 1).flatten(1, 2)
+        pos_embed = torch.cat([cls_token_pe, img_token_pe], dim=1)
+        x = self.pos_drop(x + pos_embed)
+        x = self.blocks(x)
+        x = self.norm(x)
+        return x[:, 0]
+    def forward(self, x):
+        x = self.forward_features(x)
+        x = self.head(x)
+        return x
+    def freeze_backbone(self):
+        backbone = [self.patch_embed, self.cls_token, self.pos_embed, self.pos_drop, self.blocks[:8]]
+        for module in backbone:
+            try:
+                for param in module.parameters():
+                    param.requires_grad = False
+            except:
+                module.requires_grad = False
+
+    def Unfreeze_backbone(self):
+        backbone = [self.patch_embed, self.cls_token, self.pos_embed, self.pos_drop, self.blocks[:8]]
+        for module in backbone:
+            try:
+                for param in module.parameters():
+                    param.requires_grad = True
+            except:
+                module.requires_grad = True
+
+class TwoStreamTransformer(nn.Module):
+    def __init__(self,  num_class,**kwargs):
+        super().__init__()
+        self.oneDvit = VisionTransformer()
+        self.shijue = ViT(image_size = 64,
+        patch_size = 16,
+        num_classes = 256,
+        dim = 32,
+        depth = 12,
+        heads = 16,
+        mlp_dim = 128,
+        dropout = 0.1,
+        emb_dropout = 0.1)
+        self.BN = nn.BatchNorm1d(256)
+        self.fcn = nn.Linear(256, num_class)
+    def forward(self, inputs,inputs1):
+        out1 = self.oneDvit(inputs)
+        out2 = self.shijue(inputs1)
+        concat=out1+out2
+        concat=self.BN(concat)
+        out = self.fcn(concat)
+        return out
